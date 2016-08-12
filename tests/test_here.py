85b3d1bc96ebcf03efc15d622fa3615e9d4590f8
keik <k4t0.kei@gmail.com>
2016-08-12 18:01:26 +0900

    Add tests

--- tests/test_here.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-def test_here():
-
-    assert False, 'test here'
---
+++ tests/tests.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+
+import pytest
+import numpy as np
+from audiotrans_transform_fft import FFTTransform
+
+
+def test_accept_1D_float_array_and_return_1D_complex_array():
+
+    tr = FFTTransform()
+
+    data = np.array([1, 2, 3, 4, 5])
+    transformed = tr.transform(data)
+
+    assert transformed.dtype == 'complex128', \
+        """transformed data is complex array"""
+
+    assert transformed.shape == data.shape, \
+        """transformed data is formed as same as inputed array"""
+
+
+def test_not_accept_1D_str_array_and_raise_value_error():
+
+    tr = FFTTransform()
+
+    data = np.array(['a', 'b', 'c', 'd', 'e'])
+
+    with pytest.raises(ValueError):
+        tr.transform(data)

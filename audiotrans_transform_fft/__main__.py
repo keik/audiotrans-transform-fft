60046b7bb4ccfa58295da3b8b1c27a628dfc0449
keik <k4t0.kei@gmail.com>
2016-08-15 11:26:12 +0900

    Fix to accept argv

--- audiotrans_transform_fft/__main__.py
+++ audiotrans_transform_fft/__main__.py
@@ -1,11 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from argparse import ArgumentParser, RawTextHelpFormatter
 import numpy as np
+from logging import getLogger, StreamHandler, Formatter, DEBUG
 from audiotrans import Transform
+
+logger = getLogger(__package__)
+handler = StreamHandler()
+handler.setFormatter(Formatter('[%(asctime)s %(levelname)s %(name)s] %(message)s'))
+logger.addHandler(handler)
 
 
 class FFTTransform(Transform):
 
-    def __init__(self):
-        pass
+    def __init__(self, argv):
+        parser = ArgumentParser(
+            prog=__package__,
+            description="""Transform module for FFT""",
+            formatter_class=RawTextHelpFormatter)
+
+        parser.add_argument('-v', '--verbose', dest='verbose',
+                            action='store_true',
+                            help='Run as verbose mode')
+
+        args = parser.parse_args(argv)
+
+        if args.verbose:
+            logger.setLevel(DEBUG)
+            logger.info('Start as verbose mode')
 
     def transform(self, data):
-        return np.fft.fft(data)
+        ndata = np.fft.fft(data)
+        logger.info('FFT from {} form to {} form'.format(data.shape, ndata.shape))
+        return ndata
